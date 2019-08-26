76de2d45cbadcec7adab5ba3a22dea5b0f0ab1b0
IgorZHK <rechovina@gmail.com>
2019-08-26 14:47:18 +0300

    my commit

---
+++ precache-manifest.86c5002e553f9e3ca039cb1aff8616a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8158e53dcdf4f830b893141efef2ae27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "036c74e5998298ef07e5",
+    "url": "/static/css/main.fd85d676.chunk.css"
+  },
+  {
+    "revision": "99cfe8942c86e9df9226",
+    "url": "/static/js/2.4bb086fb.chunk.js"
+  },
+  {
+    "revision": "036c74e5998298ef07e5",
+    "url": "/static/js/main.025994b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
